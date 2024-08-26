6400fca136377060310c4966dfe0b04e30b4b1ca
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-08-26 16:03:19 +0000

    Class toggle

--- js/index.header-text.js
+++ js/index.header-text.js
@@ -11,4 +11,11 @@ const greetings = [
 
 const getRandomEl = (arr) => arr[ Math.floor(Math.random() * arr.length) ];
 
+// Не рекомендовано
+// headerTitle.style.color = 'red';
+
+const toggleHighlight = () => headerTitle.classList.toggle('highlight');
+setInterval(toggleHighlight, 2000);
+
+
 headerTitle.innerText = getRandomEl(greetings);
